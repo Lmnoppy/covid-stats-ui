7d253e3a633ac16bd9161b00b8d392dbd427a68c
Lmnoppy <Lmnoppy@users.noreply.github.com>
2021-06-26 21:28:42 +0100

    added basic pages
    Added empty basic pages to help set up layout

--- src/App.js
+++ src/App.js
@@ -1,57 +1,61 @@
 import React from 'react';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
 
 //App
-import Login from "./components/Login";
+import Login from "./components/user/Login";
 import AuthProvider from "./contexts/AuthContext";
 import PrivateRoute from "./utils/PrivateRoute";
-import Signup from "./components/Signup";
-import Dashboard from "./components/Dashboard";
-import ForgotPassword from "./components/ForgotPassword";
-import UpdateAccount from "./components/UpdateAccount";
+import Signup from "./components/user/Signup";
+import Dashboard from "./pages/Dashboard";
+import ForgotPassword from "./components/user/ForgotPassword";
+import UpdateAccount from "./components/user/UpdateAccount";
+import BlogList from "./components/blog/list/BlogList";
+import HealthCheck from "./components/health/HealthCheck";
+import Home from "./pages/Home";
 
 function App() {
-  return (<Router>
-      <div className="App">
-        <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
-          <div className="container">
-            <Link className="navbar-brand" to={"/login"}>Octo Broccoli</Link>
-            <div className="collapse navbar-collapse" id="navbar01">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <Link className="nav-link" to="/login">Sign in</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/signup">Sign up</Link>
-                </li>
-                <li className="nav-item">
-                  <Link className="nav-link" to="/">My Profile</Link>
-                </li>
-              </ul>
+    return (<Router>
+            <div className="App">
+                <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
+                    <div className="container">
+                        <Link className="navbar-brand" to={"/"}>Octo Broccoli</Link>
+                        <div className="collapse navbar-collapse" id="navbar01">
+                            <ul className="navbar-nav ml-auto">
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/login">Sign in</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/signup">Sign up</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/">My Profile</Link>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+
+                <AuthProvider>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/blogList" component={BlogList}/>
+                        <Route path="/health" component={HealthCheck}/>
+
+                        <Route path="/signup" component={Signup}/>
+                        <Route path="/login" component={Login}/>
+                        <Route path="/forgot-password" component={ForgotPassword}/>
+
+                        <PrivateRoute path="/dashboard" component={Dashboard}/>
+                        <PrivateRoute path="/my-profile" component={UpdateAccount}/>
+                        <PrivateRoute path="/update-account" component={UpdateAccount}/>
+                    </Switch>
+                </AuthProvider>
+
             </div>
-          </div>
-        </nav>
-
-          <div className="outer">
-            <div className="inner">
-              <AuthProvider>
-                <Switch>
-
-                  <Route path="/signup" component={Signup} />
-                  <Route path="/login" component={Login} />
-                  <Route path="/forgot-password" component={ForgotPassword} />
-
-                  <PrivateRoute exact path="/" component={Dashboard} />
-                  <PrivateRoute path="/update-account" component={UpdateAccount} />
-                </Switch>
-              </AuthProvider>
-            </div>
-          </div>
-      </div>
-  </Router>
-        );
+        </Router>
+    );
 }
 
 export default App;
---
+++ src/components/health/HealthCheck.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function HealthCheck() {
+
+    return (
+        <>
+            <h1>A OK!</h1>
+        </>
+    )
+}
