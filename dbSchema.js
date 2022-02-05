73c7bca539da7585f7c47fe7695c77392c767a78
= <=>
2022-02-05 22:21:54 +0000

    fixes, i guess.

--- dbSchema.js
+++ dbSchema.js
@@ -10,5 +10,32 @@ let db = {
         {
 
         }
+    ], 
+    user: [
+        {
+            credentials: {
+                userId: 'abc',
+                userName: 'user',
+                password: '',
+                email: 'user@email.com',
+                isActive: true,
+                createdAt: '2020-11-22T11:08:02.805Z',
+                imageUrl: 'bla/bla/bla',
+                lastLoggedIn:'2020-11-22T11:08:02.805Z',
+            },
+            userInfo: {
+                firstName:'firstName',
+                lastName:'lastName',
+                bio: 'Hello, my name is user, nice to meet you',
+                website: 'website',
+                youtubeId: 'website',
+                twitchId: 'website',
+                twitterId: 'website',
+                discordId: 'website',
+                location: 'Glasgow, UK',
+            },
+            userStats: {
+            }
+        }
     ]
 }
