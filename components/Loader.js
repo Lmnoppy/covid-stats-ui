b049288fd4b214fcfb8b74ac6ec806d9d07adaa9
lmnoppy <code@lmnoppy.com>
2022-02-11 20:59:09 +0000

    clean up

--- components/Loader.js
+++
@@ -1,9 +0,0 @@
-export default function Loader({ show }){
-    return show ? 
-    <div className="d-flex justify-content-center">
-        <div className="spinner-grow" role="status">
-            <span className="visually-hidden">Loading...</span>
-        </div>
-    </div> 
-    : null;
-}
