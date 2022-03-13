e944572e4c4ffe4cec19ede71ade7f9ff66dcd37
lmnoppy <code@lmnoppy.com>
2022-03-13 21:48:13 +0000

    cleanup

--- components/utils/Date.js
+++ components/utils/Date.js
@@ -1,6 +1,5 @@
-import { parseISO, format } from 'date-fns'
 
-export function sortByDateMostRecent(data) {
+export default function sortByDateMostRecent(data) {
   const sortedDate = data.sort(function (a, b) {
     return new Date(b.date) - new Date(a.date);
   })
